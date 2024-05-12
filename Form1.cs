5e17eefca7383a90036653ca0c8eea43de725c7f
EMP3R0RR <168358160+EMP3R0RR@users.noreply.github.com>
2024-05-12 23:00:05 +0600

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,22 @@
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Login f2 = new Login();
+            f2.ShowDialog();
+
+        }
+    }
+}
